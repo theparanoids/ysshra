ace1e38c3fb8279cdf30a55b0c84da68552c60c4
Po-Yao Chen <alan4chen@gmail.com>
2023-03-07 23:29:53 +0800

    add

---
+++ sshutils/key/algo_test.go
@@ -0,0 +1,27 @@
+package key
+
+import "testing"
+
+func TestGetSSHKeyAlgo(t *testing.T) {
+	t.Parallel()
+	table := map[string]struct {
+		keyType     string
+		wantKeyAlgo PublicKeyAlgo
+		wantErr     bool
+	}{
+		"rsa2048 key":  {"RSA2048", RSA2048, false},
+		"ec p-256 key": {"ECCP256", ECDSAsecp256r1, false},
+		"ec p-384 key": {"ECCP384", ECDSAsecp384r1, false},
+		"invalid key":  {"ECCP521", RSA2048, true},
+		"empty key":    {"ED25519", RSA2048, true},
+	}
+	for label, tt := range table {
+		got, err := GetSSHKeyAlgo(tt.keyType)
+		if tt.wantErr && err == nil {
+			t.Errorf("%s, go nil, want err", label)
+		}
+		if tt.wantKeyAlgo != got {
+			t.Errorf("%s, got %v, want %v", label, got, tt.wantKeyAlgo)
+		}
+	}
+}
